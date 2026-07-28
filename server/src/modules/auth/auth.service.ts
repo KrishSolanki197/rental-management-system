ccad628aa45c446050bb11bbb0a708cd9604d2af
Harmin Vekariya <harminv251@gmail.com>
2026-07-28 18:49:58 +0530

    Auth files are customized

---
+++ server/src/modules/auth/auth.service.ts
@@ -0,0 +1,11 @@
+
+import { PrismaClient } from "@prisma/client";
+
+export const registerUser = async (req: Request, res: Response)=>{
+
+    const User = ""
+    
+    return {
+        User
+    }
+}
