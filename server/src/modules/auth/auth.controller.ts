ebff5fc1ad00b171afce6d329adfe62b137da819
Harmin Vekariya <harminv251@gmail.com>
2026-07-29 23:15:59 +0530

    Register Auth handle errors and transaction control (partially)

--- server/src/modules/auth/auth.controller.ts
+++ server/src/modules/auth/auth.controller.ts
@@ -2,24 +2,27 @@
 import type { Request, Response } from "express";
 import * as authService from './auth.service.js';
 import type { RegisterUserInput } from "../../types/User.js";
+import type { ResponseType } from "../../types/ResponseType.js";
 
 export const register = async (req: Request, res: Response): Promise<void>  => {
 
-  const { username, firstname, lastname, phone, email, password } = req.body;
-
+  // format the payload before send to the service layer 
   const payload:RegisterUserInput = { 
-    username: username.toLowerCase().trim(), 
-    firstname: firstname.toLowerCase().trim(), 
-    lastname: lastname.trim().toLowerCase(), 
-    phone: phone.trim(), 
-    email: email.trim().toLowerCase(), 
-    password: password.toLowerCase().trim() 
+    username: req.body.username.toLowerCase().trim(), 
+    firstname: req.body.firstname.toLowerCase().trim(), 
+    lastname: req.body.lastname.trim().toLowerCase(), 
+    phone: req.body.phone.trim(), 
+    email: req.body.email.trim().toLowerCase(), 
+    password: req.body.password.toLowerCase().trim() 
   };
 
   const result = await authService.registerUser(payload);
 
+  if(result.success){
+    res.status(200).send(result)
+  }
 
-  res.status(200).send(result);
+  res.status(400).send(result);
 };
 
 export const login = async (req: Request, res: Response) => {
--- server/src/modules/auth/auth.service.ts
+++ server/src/modules/auth/auth.service.ts
@@ -1,56 +1,116 @@
-
-
-import type { JsonObject } from "@prisma/client/runtime/library";
+// libraries
 import prisma from "../../config/prisma.js";
-import sendregistationmail from "../../utils/mail.js";
-import type { RegisterUserInput, User } from "../../types/User.js";
+import bcrypt from "bcrypt";
+
+// utils functions
+import send_register_mail from "../../utils/mail.js";
+
+// custom types
+import type { RegisterUserInput } from "../../types/User.js";
 import type { ResponseType } from "../../types/ResponseType.js";
-import bcrypt from 'bcrypt';
+// prisma generate type for its each reletions
+import type { users } from "@prisma/client";
 
+/**
+ *
+ * @param payload
+ * @returns
+ * @description this is method is return the user detail if the new user was created
+ *
+ * 1. Get Payload
+ * 2. Verify Payload data
+ * 3. Check User Exist Already, If yes then return "user already exists"
+ * 4. generate password hash
+ * 5. save the user data
+ * 6. new token for that user
+ * 7. send mail to the user
+ * 8. send details to the client
+ */
 export const registerUser = async (payload: RegisterUserInput): Promise<ResponseType> => {
+  try {
+    // destructure the paylaod 
+    const { username, firstname, lastname, phone, email, password } = payload;
 
-    const {
-        username, 
-        firstname, lastname,
-        phone, email, password
-    }  = payload; 
+    // check for the user email 
+    const userExist: users | null = await prisma.users.findUnique({
+        where: {
+            email
+        },
+    });
+    if (userExist) throw new Error("user already exists");
+    
+    // check for the username 
+    const usernameExists: users | null = await prisma.users.findUnique({
+        where: {
+            username
+        },
+    });
+    if (usernameExists) throw new Error("username already exists");
+    
+    // hash the password
+    const hashed_password = await bcrypt.hash(password, 10);
 
-    const password_hash = await bcrypt.hash(password, 10);
+    // create user 
+    const user: users = await prisma.$transaction(async (tx) => {
+      const _user = await tx.users.create({
+        data: {
+          username,
+          phone,
+          email,
+          password_hash: hashed_password,
+          
+        },
+      });
 
-    const user:RegisterUserInput = await prisma.$transaction(async (tx)=>{
-        const _user = await tx.users.create({
-            data:{
-                username, phone, email, password_hash
-            },
-        })
+      await tx.customers.create({
+        data: {
+          user_id: _user.user_id,
+          first_name: firstname,
+          last_name: lastname,
+          
+        },
+      });
 
-        await tx.customers.create({
-            data:{
-                user_id: _user.user_id,
-                first_name: firstname,
-                last_name: lastname
-            }
-        })
+      const role = await tx.roles.findUnique({
+        where: {
+          role_name: "CUSTOMER",
+        },
+      });
 
-        await tx.user_roles.create({
-            data:{
-                user_id: _user.user_id,
-                role_id: 5
-            }
-        })
+      if (!role) {
+        throw new Error("CUSTOMER role not found!");
+      }
 
-        return { ..._user }
-    })
+      await tx.user_roles.create({
+        data: {
+          user_id: _user.user_id,
+          role_id: role.role_id,
+        },
+      });
+      return { ..._user };
+    });
 
+    // send email to tell that the new user create using this mail 
+    try {
+        await send_register_mail(email);
+    }catch(error){
+        console.error(error);
+    }
 
+    const { password_hash, ...safeUser} = user;
 
     return {
-        success: true,
-        message: "User is saved",
-        data: {
-            username: user.username,
+      success: true,
+      message: "User is saved",
+      data: safeUser
+    };
 
-        }
-    }
-    
+  } catch (error: unknown) {
+    console.error(error.name);
+    return {
+      success: false,
+      message: error.message,
+      data: {},
+    };
+  }
 };
